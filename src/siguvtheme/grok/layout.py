bc93c87ddbf7347b0bb7966c8ed68d6bf1b0957c
Souheil CHELFOUH <trollfot@gmail.com>
2015-05-21 15:18:50 +0200

    removed useless import

--- src/siguvtheme/grok/layout.py
+++ src/siguvtheme/grok/layout.py
@@ -4,7 +4,6 @@ import os
 import grok
 
 from . import templates_dir
-#from .resources import tune
 from uvc.tbskin.skin import ITBSkin, Layout
 from uvc.api.api import get_template
 from siguvtheme.resources import tune
